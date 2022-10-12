345ab75b611a123ca5803da18f6a275fd80ab2f5
seoyooung <sylim0318@naver.com>
2022-10-12 15:34:49 +0900

    post request

---
+++ src/main/java/com/example/study/payload/request/PostRequest.java
@@ -0,0 +1,12 @@
+package com.example.study.payload.request;
+
+import lombok.Getter;
+
+@Getter
+public class PostRequest {
+
+    private String title;
+    private String content;
+    private String writer;
+
+}
