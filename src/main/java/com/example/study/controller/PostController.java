2b5d56abdff6edecde12179c48d3912fe2f9158b
seoyooung <sylim0318@naver.com>
2022-10-12 15:31:34 +0900

    post controller

---
+++ src/main/java/com/example/study/controller/PostController.java
@@ -0,0 +1,44 @@
+package com.example.study.controller;
+
+import com.example.study.payload.request.PostRequest;
+import com.example.study.payload.response.PostResponse;
+import com.example.study.service.PostService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/post")
+public class PostController {
+
+    private final PostService postService;
+
+    @PostMapping
+    public void creatPost(@RequestBody PostRequest request) {
+        postService.createPost(request);
+    }
+
+    @PatchMapping("/{postId}")
+    public void updatePost(@PathVariable Integer postId,
+                           @RequestBody PostRequest request) {
+        postService.updatePost(request, postId);
+    }
+
+    @GetMapping
+    public List<PostResponse> getPostList() {
+        return postService.getPostList();
+    }
+
+    @GetMapping("/{postId}")
+    public PostResponse getPost(@PathVariable Integer postId) {
+        return postService.getPost(postId);
+    }
+
+    @DeleteMapping("/{postId}")
+    public void deletePost(@PathVariable Integer postId) {
+        postService.deletePost(postId);
+    }
+
+}
