2e10a8041a33df0a8cac9ccbcfe8971154abf195
seoyooung <sylim0318@naver.com>
2022-10-12 15:31:44 +0900

    post entity

---
+++ src/main/java/com/example/study/entity/Post.java
@@ -0,0 +1,43 @@
+package com.example.study.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "post")
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String title;
+
+    private String content;
+
+    private String writer;
+
+    @Column(name = "created_at")
+    private LocalDate createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDate updatedAt;
+
+    public Post updatePost(String title, String content, String writer, LocalDate updatedAt) {
+        this.title = title;
+        this.content = content;
+        this.writer = writer;
+        this.updatedAt = updatedAt;
+
+        return this;
+    }
+
+}
