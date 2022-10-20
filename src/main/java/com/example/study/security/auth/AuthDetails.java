8481943a8f91b1a53b6e97741aeacf8b8f2e3da5
seoyooung <66578746+lliimm318@users.noreply.github.com>
2022-10-20 09:30:04 +0900

    [ADD] auth details

---
+++ src/main/java/com/example/study/security/auth/AuthDetails.java
@@ -0,0 +1,50 @@
+package com.example.study.security.auth;
+
+import com.example.study.entity.User;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+@AllArgsConstructor
+public class AuthDetails implements UserDetails {
+
+    private final User user;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return new ArrayList<>();
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getId();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
---
+++ src/main/java/com/example/study/security/auth/AuthDetailsService.java
@@ -0,0 +1,22 @@
+package com.example.study.security.auth;
+
+import com.example.study.exception.UserNotFoundException;
+import com.example.study.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public AuthDetails loadUserByUsername(String id) throws UsernameNotFoundException {
+        return userRepository.findById(id)
+                .map(AuthDetails::new)
+                .orElseThrow(UserNotFoundException::new);
+    }
+}
