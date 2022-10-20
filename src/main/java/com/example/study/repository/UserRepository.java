b835c44d52dcc693a7b46586ffe1c1ef58dd5697
seoyooung <66578746+lliimm318@users.noreply.github.com>
2022-10-20 09:30:13 +0900

    [ADD] user repository

---
+++ src/main/java/com/example/study/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.example.study.repository;
+
+import com.example.study.entity.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, String> {
+
+    Optional<User> findByName(String name);
+
+}
