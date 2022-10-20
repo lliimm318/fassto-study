f73065efdac181a000275daa604e2bbe4a3e9ee8
seoyooung <66578746+lliimm318@users.noreply.github.com>
2022-10-20 09:30:29 +0900

    [ADD] token response

---
+++ src/main/java/com/example/study/payload/response/TokenResponse.java
@@ -0,0 +1,15 @@
+package com.example.study.payload.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class TokenResponse {
+
+    private String accessToken;
+    private String refreshToken;
+
+}
