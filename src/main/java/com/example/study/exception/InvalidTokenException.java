92b10ba71126702be859fdf461c78b07eaa38a67
seoyooung <66578746+lliimm318@users.noreply.github.com>
2022-11-08 10:00:00 +0900

    [DELETE]

--- src/main/java/com/example/study/exception/InvalidTokenException.java
+++
@@ -1,8 +0,0 @@
-package com.example.study.exception;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(value = HttpStatus.UNAUTHORIZED, reason = "invalid token")
-public class InvalidTokenException extends RuntimeException {
-}
