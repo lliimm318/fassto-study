d8890bf37bd6ed484eb357a25b61e34a9d581fa6
seoyooung <66578746+lliimm318@users.noreply.github.com>
2022-11-08 10:00:03 +0900

    [DELETE]

--- src/main/java/com/example/study/exception/PostNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.example.study.exception;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "post not found")
-public class PostNotFoundException extends RuntimeException {
-}
