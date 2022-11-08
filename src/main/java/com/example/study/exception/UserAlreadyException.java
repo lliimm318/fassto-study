fd1027da9bfc2a6ce57692fda8af33f7966c7292
seoyooung <66578746+lliimm318@users.noreply.github.com>
2022-11-08 10:00:05 +0900

    [DELETE]

--- src/main/java/com/example/study/exception/UserAlreadyException.java
+++
@@ -1,8 +0,0 @@
-package com.example.study.exception;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(code = HttpStatus.CONFLICT, reason = "user already")
-public class UserAlreadyException extends RuntimeException {
-}
