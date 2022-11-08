08cb202dfa6bdf0d2069b0c7aff5be0efcd10829
seoyooung <66578746+lliimm318@users.noreply.github.com>
2022-11-08 10:19:03 +0900

    [ADD] error code

---
+++ src/main/java/com/example/study/exception/ErrorCode.java
@@ -0,0 +1,21 @@
+package com.example.study.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum ErrorCode {
+
+    INVALID_TOKEN(401, "invalid token"),
+    USER_NOT_ACCESS(403, "user not access"),
+    POST_NOT_FOUND(404,"user not found"),
+    USER_NOT_FOUND(404,"user not found"),
+    USER_ID_ALREADY(409, "user id already"),
+    USER_NAME_ALREADY(409, "user name already");
+
+    private final int status;
+
+    private final String message;
+
+}
