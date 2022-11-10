0c8cf9441e655e813aff3f567a06e305ce8cf6b9
seoyooung <66578746+lliimm318@users.noreply.github.com>
2022-11-10 13:35:03 +0900

    [FIX] restcontrolleradvice로 변경

--- src/main/java/com/example/study/exception/BaseExceptionHandler.java
+++ src/main/java/com/example/study/exception/BaseExceptionHandler.java
@@ -8,41 +8,67 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDateTime;
 
 @ControllerAdvice
+@RestControllerAdvice
 public class BaseExceptionHandler {
 
     @ExceptionHandler(BaseException.class)
-    protected ResponseEntity<ErrorResponse> baseExceptionHandle(final BaseException e) {
+    protected ResponseEntity<ErrorResponse> baseExceptionHandle(final BaseException e, HttpServletRequest request) {
         final ErrorCode errorCode = e.getErrorCode();
-        return new ResponseEntity<>(new ErrorResponse(errorCode.getStatus(), errorCode.getMessage()),
+        return new ResponseEntity<>(new ErrorResponse(
+                LocalDateTime.now(),
+                errorCode.getStatus(),
+                errorCode.getMessage(),
+                request.getServletPath()),
                 HttpStatus.valueOf(errorCode.getStatus()));
     }
 
     @ExceptionHandler(HttpMessageNotReadableException.class)
-    protected ResponseEntity<ErrorResponse> exceptionHandler(final Exception e) {
-        return new ResponseEntity<>(new ErrorResponse(400, e.getMessage()),
+    protected ResponseEntity<ErrorResponse> exceptionHandler(final Exception e, HttpServletRequest request) {
+
+        return new ResponseEntity<>(new ErrorResponse(
+                LocalDateTime.now(),
+                400,
+                e.getMessage(),
+                request.getServletPath()),
                 HttpStatus.valueOf(400));
     }
 
     //validation @RequestBody
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    protected ResponseEntity<ErrorResponse> exceptionHandler(final MethodArgumentNotValidException e) {
-        return new ResponseEntity<>(new ErrorResponse(404, e.getMessage()),
+    protected ResponseEntity<ErrorResponse> exceptionHandler(final MethodArgumentNotValidException e, HttpServletRequest request) {
+        return new ResponseEntity<>(new ErrorResponse(
+                LocalDateTime.now(),
+                404,
+                e.getMessage(),
+                request.getServletPath()),
                 HttpStatus.valueOf(404));
     }
 
     //@RequestParam
     @ExceptionHandler(MissingServletRequestParameterException.class)
-    protected ResponseEntity<ErrorResponse> exceptionHandler(final MissingServletRequestParameterException e) {
-        return new ResponseEntity<>(new ErrorResponse(404, e.getMessage()),
+    protected ResponseEntity<ErrorResponse> exceptionHandler(final MissingServletRequestParameterException e, HttpServletRequest request) {
+        return new ResponseEntity<>(new ErrorResponse(
+                LocalDateTime.now(),
+                404,
+                e.getMessage(),
+                request.getServletPath()),
                 HttpStatus.valueOf(404));
     }
 
     @ExceptionHandler(Exception.class)
-    protected ResponseEntity<ErrorResponse> exceptionHandle(final Exception e) {
+    protected ResponseEntity<ErrorResponse> exceptionHandle(final Exception e, HttpServletRequest request) {
         //e.printStackTrace();
-        return new ResponseEntity<>(new ErrorResponse(500, e.getMessage()),
+        return new ResponseEntity<>(new ErrorResponse(
+                LocalDateTime.now(),
+                500,
+                e.getMessage(),
+                request.getServletPath()),
                 HttpStatus.valueOf(500));
     }
 
