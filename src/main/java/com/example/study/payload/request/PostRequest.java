cb5771351663a546b63764815b0beffe99d0d910
seoyooung <66578746+lliimm318@users.noreply.github.com>
2022-11-10 13:33:40 +0900

    [UPDATE] validation 설정 추가

--- src/main/java/com/example/study/payload/request/PostRequest.java
+++ src/main/java/com/example/study/payload/request/PostRequest.java
@@ -1,18 +1,23 @@
 package com.example.study.payload.request;
 
+import com.example.study.validation.MinMaxGroup;
+import com.example.study.validation.NotEmptyGroup;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.Max;
 import javax.validation.constraints.NotBlank;
 
 @Getter
 @Setter
 public class PostRequest {
 
-    @NotBlank
+    @NotBlank(message = "제목은 필수 입력값 입니다.", groups = NotEmptyGroup.class)
+    @Max(value = 100, message = "제목은 100자 이하여야 합니당", groups = MinMaxGroup.class)
     private String title;
 
-    @NotBlank
+    @NotBlank(message = "내용은 필수 입력값 입니다.", groups = NotEmptyGroup.class)
+    @Max(value = 100, message = "내용은 1000자 이하여야 합니당", groups = MinMaxGroup.class)
     private String content;
 
 }
