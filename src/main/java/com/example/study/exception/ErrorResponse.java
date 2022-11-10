60c92feeb2aac3564ce4909e42b1a64431efdd3b
seoyooung <66578746+lliimm318@users.noreply.github.com>
2022-11-10 13:34:31 +0900

    [UPDATE] error 자세하게

--- src/main/java/com/example/study/exception/ErrorResponse.java
+++ src/main/java/com/example/study/exception/ErrorResponse.java
@@ -1,16 +1,27 @@
 package com.example.study.exception;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
+
+import java.time.LocalDateTime;
 
 @Getter
 public class ErrorResponse {
 
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss z", timezone = "Asia/Seoul")
+    private LocalDateTime dateTime;
+
     private final int status;
+
     private final String message;
 
-    ErrorResponse(int status, String message) {
+    private final String url;
+
+    ErrorResponse(LocalDateTime dateTime, int status, String message, String url) {
+        this.dateTime = dateTime;
         this.status = status;
         this.message = message;
+        this.url = url;
     }
 
 }
