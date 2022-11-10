bd74fe9e972ae8e3b31cd63e7b7de66424e270ba
seoyooung <66578746+lliimm318@users.noreply.github.com>
2022-11-10 13:34:44 +0900

    [FIX] 오타 수정

--- src/main/java/com/example/study/exception/ErrorCode.java
+++ src/main/java/com/example/study/exception/ErrorCode.java
@@ -9,7 +9,7 @@ public enum ErrorCode {
 
     INVALID_TOKEN(401, "invalid token"),
     USER_NOT_ACCESS(403, "user not access"),
-    POST_NOT_FOUND(404,"user not found"),
+    POST_NOT_FOUND(404,"post not found"),
     USER_NOT_FOUND(404,"user not found"),
     USER_ID_ALREADY(409, "user id already"),
     USER_NAME_ALREADY(409, "user name already");
