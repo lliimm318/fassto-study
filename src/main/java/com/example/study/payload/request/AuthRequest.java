c117527fdf7d0feb187ecf2e0932d5ed2bfd21fa
seoyooung <66578746+lliimm318@users.noreply.github.com>
2022-11-10 13:33:45 +0900

    [UPDATE] validation 설정 추가

--- src/main/java/com/example/study/payload/request/AuthRequest.java
+++ src/main/java/com/example/study/payload/request/AuthRequest.java
@@ -1,5 +1,6 @@
 package com.example.study.payload.request;
 
+import com.example.study.validation.NotEmptyGroup;
 import lombok.Getter;
 
 import javax.validation.constraints.NotNull;
@@ -7,10 +8,10 @@ import javax.validation.constraints.NotNull;
 @Getter
 public class AuthRequest {
 
-    @NotNull
+    @NotNull(message = "아이디는 필수 입력값 입니다.", groups = NotEmptyGroup.class)
     private String id;
 
-    @NotNull
+    @NotNull(message = "비밀반호는 필수 입력값 입니다.", groups = NotEmptyGroup.class)
     private String password;
 
 }
