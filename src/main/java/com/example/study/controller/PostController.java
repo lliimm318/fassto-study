c0df5dcad6731b4ee84a9b83420236060fc2c7db
seoyooung <66578746+lliimm318@users.noreply.github.com>
2022-11-10 13:33:57 +0900

    [UPDATE] validation 설정 추가

--- src/main/java/com/example/study/controller/PostController.java
+++ src/main/java/com/example/study/controller/PostController.java
@@ -3,10 +3,14 @@ package com.example.study.controller;
 import com.example.study.payload.request.PostRequest;
 import com.example.study.payload.response.PostResponse;
 import com.example.study.domain.service.PostService;
+import com.example.study.validation.NotEmptyGroup;
+import com.example.study.validation.PositiveGroup;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
 import java.util.List;
 
 @RestController
@@ -22,7 +26,10 @@ public class PostController {
     }
 
     @PatchMapping("/{postId}")
-    public void updatePost(@PathVariable Integer postId,
+    public void updatePost(@PathVariable
+                           @NotNull(message = "게시글 Id는 필수 항목 입니다.", groups = NotEmptyGroup.class)
+                           @Positive(message = "게시글 Id는 양수여야 합니다.", groups = PositiveGroup.class)
+                           Integer postId,
                            @Validated @RequestBody PostRequest request) {
         postService.updatePost(request, postId);
     }
@@ -33,12 +40,18 @@ public class PostController {
     }
 
     @GetMapping("/{postId}")
-    public PostResponse getPost(@PathVariable Integer postId) {
+    public PostResponse getPost(@PathVariable
+                                @NotNull(message = "게시글 Id는 필수 항목 입니다.", groups = NotEmptyGroup.class)
+                                @Positive(message = "게시글 Id는 양수여야 합니다.", groups = PositiveGroup.class)
+                                Integer postId) {
         return postService.getPost(postId);
     }
 
     @DeleteMapping("/{postId}")
-    public void deletePost(@PathVariable Integer postId) {
+    public void deletePost(@PathVariable
+                           @NotNull(message = "게시글 Id는 필수 항목 입니다.", groups = NotEmptyGroup.class)
+                           @Positive(message = "게시글 Id는 양수여야 합니다.", groups = PositiveGroup.class)
+                           Integer postId) {
         postService.deletePost(postId);
     }
 
