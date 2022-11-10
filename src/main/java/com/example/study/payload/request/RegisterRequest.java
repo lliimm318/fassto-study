7d583c17c0f1ca5ee0f0408d7570f6b616f86bc9
seoyooung <66578746+lliimm318@users.noreply.github.com>
2022-11-10 13:33:37 +0900

    [UPDATE] validation 설정 추가

--- src/main/java/com/example/study/payload/request/RegisterRequest.java
+++ src/main/java/com/example/study/payload/request/RegisterRequest.java
@@ -1,19 +1,24 @@
 package com.example.study.payload.request;
 
+import com.example.study.validation.MinMaxGroup;
+import com.example.study.validation.NotEmptyGroup;
 import lombok.Getter;
 
+import javax.validation.constraints.Max;
 import javax.validation.constraints.NotNull;
 
 @Getter
 public class RegisterRequest {
 
-    @NotNull
+    @NotNull(message = "아이디는 필수 입력값 입니다.", groups = NotEmptyGroup.class)
+    @Max(value = 50, message = "아이디는 50자 이하여야 합니당", groups = MinMaxGroup.class)
     private String id;
 
-    @NotNull
+    @NotNull(message = "닉네임은 필수 입력값 입니다.", groups = NotEmptyGroup.class)
+    @Max(value = 10, message = "닉네임은 10자 이하여야 합니당", groups = MinMaxGroup.class)
     private String name;
 
-    @NotNull
+    @NotNull(message = "비밀번호는 필수 입력값 입니다.", groups = NotEmptyGroup.class)
     private String password;
 
 }
